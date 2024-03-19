ef6471f0b59e39e457fed0baba5d0812b884019f
Ines <60435099+Inesseni@users.noreply.github.com>
2024-03-19 19:19:23 +0100

    test4

--- src/ImgComponent.js
+++ src/ImgComponent.js
@@ -7,7 +7,15 @@ const ImgComponent = (props) => {
   const randomTop = Math.floor(Math.random() * maxPageHeight); // Example: Generate a random number between 0 and 1000
   const randomLeft = Math.floor(Math.random() * 80); // Example: Generate a random number between 0 and 100
 
-  return <img src={props.src} alt="note" width="500" height="600"></img>;
+  return (
+    <img
+      src={props.src}
+      alt="note"
+      width="500"
+      height="600"
+      style={{ top: randomTop, left: `${randomLeft}%` }}
+    ></img>
+  );
 };
 
 export default ImgComponent;
