b8f802a95341a17c1e453844250d57edc06a5f90
Ines <60435099+Inesseni@users.noreply.github.com>
2024-03-23 14:40:36 +0100

    generating random positions in function

--- src/ImgComponent.js
+++ src/ImgComponent.js
@@ -1,34 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 const maxPageHeight = 5000;
-
-function randomNormalDistribution(mean, deviation) {
-  let u = 0,
-    v = 0;
-  while (u === 0) u = Math.random(); // Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random();
-  const num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-  return num * deviation + mean;
-}
 
 // A functional component
 const ImgComponent = (props) => {
-  const randomTop = Math.floor(Math.random() * maxPageHeight) - 300; // Example: Generate a random number between 0 and 1000
-  const randomLeft = Math.floor(Math.random() * props.maxLeft) - props.minLeft; // Example: Generate a random number between 0 and 100
-  /*
-  
-  const windowHeight = maxPageHeight; // Height of the window
-  const meanTop = 0; // Centering around the vertical middle axis
-  const deviationTop = windowHeight / 4; // Adjust this value for the desired spread
-  
-  // Generate random top position centered around the middle of the page
-  //const randomTop = randomNormalDistribution(meanTop, deviationTop);
-  
-  // Generate random left position (you can adjust this as needed)
-  const meanLeft = 0; // Centered horizontally
-  const deviationLeft = 800; // Adjust this value for the desired spread
-  const randomLeft = randomNormalDistribution(meanLeft, deviationLeft);
-  */
+  const [randomTop, setRandomTop] = useState(0);
+  const [randomLeft, setRandomLeft] = useState(0);
+
+  useEffect(() => {
+    // Function to calculate random top and left values
+    const calculateRandomPosition = () => {
+      const randomTopValue = Math.floor(Math.random() * maxPageHeight) - 300;
+      const randomLeftValue =
+        Math.floor(Math.random() * props.maxLeft) - props.minLeft;
+
+      setRandomTop(randomTopValue);
+      setRandomLeft(randomLeftValue);
+    };
+
+    // Call the function when component mounts
+    calculateRandomPosition();
+  }, [props.maxLeft, props.minLeft]);
 
   return (
     <img
